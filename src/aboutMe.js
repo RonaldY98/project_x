b02531b2149c8c830b4eebbda3f29eadde6bfcc3
RonaldY98 <16tfry@queensu.ca>
2023-10-01 15:29:24 -0400

    Changes to text sections

--- src/aboutMe.js
+++ src/aboutMe.js
@@ -4,65 +4,38 @@ import "./main.css";
 class AboutMe extends Component {
   render() {
     return (
-      <div className="text-white mb-20" id="section_2">
-        <h2 className="text-yellow-200">ABOUT ME</h2>
-        <p>
-          Cras facilisis urna ornare ex volutpat, et convallis erat elementum.
-          Ut aliquam, ipsum vitae gravida suscipit, metus dui bibendum est, eget
-          rhoncus nibh metus nec massa. Maecenas hendrerit laoreet augue nec
-          molestie. Cum sociis natoque penatibus et magnis dis parturient
-          montes, nascetur ridiculus mus.
-        </p>
-        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
-        <p>
-          Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at
-          interdum magna augue eget diam. Vestibulum ante ipsum primis in
-          faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia
-          molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet
-          augue congue elementum. Morbi in ipsum sit amet pede facilisis
-          laoreet. Donec lacus nunc, viverra nec, blandit vel, egestas et,
-          augue. Vestibulum tincidunt malesuada tellus. Ut ultrices ultrices
-          enim. Curabitur sit amet mauris. Morbi in dui quis est pulvinar
-          ullamcorper. Nulla facilisi. Sticky Heading 2 Integer lacinia
-          sollicitudin massa. Cras metus. Sed aliquet risus a tortor. Integer id
-          quam. Morbi mi. Quisque nisl felis, venenatis tristique, dignissim in,
-          ultrices sit amet, augue. Proin sodales libero eget ante. Nulla quam.
-          Aenean laoreet. Vestibulum nisi lectus, commodo ac, facilisis ac,
-          ultricies eu, pede. Ut orci risus, accumsan porttitor, cursus quis,
-          aliquet eget, justo. Sed pretium blandit orci. Ut eu diam at pede
-          suscipit sodales. Aenean lectus elit, fermentum non, convallis id,
-          sagittis at, neque. Sticky Heading 3 Nullam mauris orci, aliquet et,
-          iaculis et, viverra vitae, ligula. Nulla ut felis in purus aliquam
-          imperdiet. Maecenas aliquet mollis lectus. Vivamus consectetuer risus
-          et tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed
-          nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
-          Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris
-          massa. Sticky Heading 4 Quisque cursus, metus vitae pharetra auctor,
-          sem massa mattis sem, at interdum magna augue eget diam. Vestibulum
-          ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
-          Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non
-          quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit
-          amet pede facilisis laoreet. Donec lacus nunc, viverra nec, blandit
-          vel, egestas et, augue. Vestibulum tincidunt malesuada tellus. Ut
-          ultrices ultrices enim. Curabitur sit amet mauris. Morbi in dui quis
-          est pulvinar ullamcorper. Nulla facilisi. Sticky Heading 5 Integer
-          lacinia sollicitudin massa. Cras metus. Sed aliquet risus a tortor.
-          Integer id quam. Morbi mi. Quisque nisl felis, venenatis tristique,
-          dignissim in, ultrices sit amet, augue. Proin sodales libero eget
-          ante. Nulla quam. Aenean laoreet. Vestibulum nisi lectus, commodo ac,
-          facilisis ac, ultricies eu, pede. Ut orci risus, accumsan porttitor,
-          cursus quis, aliquet eget, justo. Sed pretium blandit orci. Ut eu diam
-          at pede suscipit sodales. Aenean lectus elit, fermentum non, convallis
-          id, sagittis at, neque. Sticky Heading 6 Nullam mauris orci, aliquet
-          et, iaculis et, viverra vitae, ligula. Nulla ut felis in purus aliquam
-          imperdiet. Maecenas aliquet mollis lectus. Vivamus consectetuer risus
-          et tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed
-          nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
-          Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris
-          massa.
-        </p>
+      <div className="text-white mb-20 text-left w-4/6" id="section_2">
+        <h2 className="text-6xl text-yellow-200">ABOUT ME</h2>
+        <p className="text-4xl">It's Me, Hi</p>
+        <div className="text-3xl">
+          <p className="mb-5">
+            I'm a software engineer based out of <b>Toronto, ON</b> with a
+            passion for tech. My mission is to use my passion for problem
+            solving and science, and contribute to solving problems in our day
+            and age.
+          </p>
+          <p className="mb-5">
+            My background is in physics and mechanical engineering but my love
+            for software and apathy for boredom led me to the land of 1's and
+            0's. I excel in working with multi-disciplinary teams to bridge the
+            gap between hardware and software.
+          </p>
+          <p>
+            In my spare time, I make cool cocktails, work on fun side projects,
+            and give my cat <b>Oni</b> lots of pets. I accept monetary
+            donations, but only in Monopoly money. Link{" "}
+            <b>
+              <a
+                href="https://youtu.be/dQw4w9WgXcQ?si=vXbfW4Nf_2WRcTeP"
+                target="_blank"
+                rel="noreferrer"
+              >
+                here
+              </a>
+            </b>{" "}
+            to donate!
+          </p>
+        </div>
       </div>
     );
   }
--- src/contact.js
+++ src/contact.js
@@ -3,13 +3,12 @@ import React, { Component } from "react";
 class Contact extends Component {
   render() {
     return (
-      <div className="text-white my-20 mb-36" id="section_4">
-        <h2 className="text-yellow-200">CONTACT</h2>
-        <p>
-          The easiest thing to do is post on our{" "}
-          <a href="http://forum.kirupa.com">forums</a>.
+      <div className="text-white my-20 mb-36 text-left w-4/6" id="section_4">
+        <h2 className="text-6xl text-yellow-200">CONTACT</h2>
+        <p className="text-4xl">
+          Carrier pigeon is my primary mode of communication
         </p>
-        <p>
+        <div className="text-3xl">
           <p>
             Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis
             ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta.
@@ -26,20 +25,8 @@ class Contact extends Component {
             aliquet risus a tortor. Integer id quam. Morbi mi. Quisque nisl
             felis, venenatis tristique, dignissim in, ultrices sit amet, augue.
             Proin sodales libero eget ante. Nulla quam. Aenean laoreet.
-            Vestibulum nisi lectus, commodo ac, facilisis ac, ultricies eu,
-            pede. Ut orci risus, accumsan porttitor, cursus quis, aliquet eget,
-            justo. Sed pretium blandit orci. Ut eu diam at pede suscipit
-            sodales. Aenean lectus elit, fermentum non, convallis id, sagittis
-            at, neque. Sticky Heading 6 Nullam mauris orci, aliquet et, iaculis
-            et, viverra vitae, ligula. Nulla ut felis in purus aliquam
-            imperdiet. Maecenas aliquet mollis lectus. Vivamus consectetuer
-            risus et tortor. Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus
-            diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis
-            sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper
-            porta. Mauris massa.
           </p>
-        </p>
+        </div>
       </div>
     );
   }
