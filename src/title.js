e6388fcc816c80d2edd33bb0db07550e37ce2a7a
RonaldY98 <16tfry@queensu.ca>
2023-10-22 01:21:09 -0400

    Changing font type of the title

--- src/title.js
+++ src/title.js
@@ -8,8 +8,8 @@ class Title extends Component {
   render() {
     return (
       <div className="h-screen pt-96 " id="section_1">
-        <h1 className="flex justify-center text-8xl text-yellow-200 mb-3 font-inter">
-          Ronald Yeung
+        <h1 className="flex justify-center text-8xl text-yellow-200 mb-3 font-inter font-light">
+          Ronny Yeung
         </h1>
         <h3 className="flex justify-center text-3xl text-white font-bold">
           Software Engineer | Bartender | Optimist
