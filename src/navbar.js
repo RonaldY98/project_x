f99b9c7516faf9461ea4ff8f60d7182591c80499
RonaldY98 <16tfry@queensu.ca>
2023-10-22 04:06:03 -0400

    Removing home from nav

--- src/navbar.js
+++ src/navbar.js
@@ -14,7 +14,7 @@ class Navbar extends Component {
       >
         <ul className="cursor-pointer grid grid-cols-1 w-full">
           {/* To add border use 'border-l-4 border-transparent border-blue-400' */}
-          <NavItem title="HOME" scrollspyId="section_1" active={true}></NavItem>
+          {/* <NavItem title="HOME" scrollspyId="section_1" active={true}></NavItem> */}
           <NavItem
             title="ABOUT ME"
             scrollspyId="section_2"
@@ -30,20 +30,6 @@ class Navbar extends Component {
             scrollspyId="section_4"
             active={false}
           ></NavItem>
-          {/* <li className="flex flex-wrap justify-center content-center text-gray-100 text-opacity-60 bg-gray-800 bg-opacity-100 hover:bg-opacity-100 hover:text-gray-100 hover:text-opacity-60">
-            <p className="outline-none" data-to-scrollspy-id="section_1">
-              HOME
-            </p>
-          </li>
-          <li className="flex flex-wrap justify-center content-center text-gray-100 text-opacity-20 bg-gray-800 bg-opacity-90 hover:bg-opacity-100 hover:text-gray-100 hover:text-opacity-60 border-l-4 border-transparent">
-            <p data-to-scrollspy-id="section_2">ABOUT ME</p>
-          </li>
-          <li className="flex flex-wrap justify-center content-center text-gray-100 text-opacity-20 bg-gray-800 bg-opacity-90 hover:bg-opacity-100 hover:text-gray-100 hover:text-opacity-60 border-l-4 border-transparent">
-            <p data-to-scrollspy-id="section_3">PROJECTS</p>
-          </li>
-          <li className="flex flex-wrap justify-center content-center text-gray-100 text-opacity-20 bg-gray-800 bg-opacity-90 hover:bg-opacity-100 hover:text-gray-100 hover:text-opacity-60 border-l-4 border-transparent">
-            <p data-to-scrollspy-id="section_4">CONTACT</p>
-          </li> */}
         </ul>
       </div>
     );
