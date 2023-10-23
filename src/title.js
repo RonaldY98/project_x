040431064692ff04ba2d12073e75f4f3819fbfae
RonaldY98 <16tfry@queensu.ca>
2023-10-23 18:20:06 -0400

    Added bounce to title components

--- src/title.js
+++ src/title.js
@@ -12,6 +12,7 @@ class Title extends Component {
         <motion.h1
           className="flex justify-center text-8xl text-yellow-200 mb-3 font-inter font-light"
           whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
           transition={{ duration: 0.3 }}
         >
           Ronny Yeung
@@ -19,6 +20,7 @@ class Title extends Component {
         <motion.h3
           className="flex justify-center text-3xl text-white font-bold"
           whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
           transition={{ duration: 0.3 }}
         >
           Software Engineer | Bartender | Creative
@@ -26,6 +28,7 @@ class Title extends Component {
         <motion.h3
           className="flex justify-center text-white"
           whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
           transition={{ duration: 0.3 }}
         >
           The World's Your Oyster, And It's Buck a' Shuck
