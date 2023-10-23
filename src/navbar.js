80194a582d26709fcf27e18556f027b63c2cb2c5
RonaldY98 <16tfry@queensu.ca>
2023-10-23 13:48:40 -0400

    Changed navbar to be a functional motion component

--- src/navbar.js
+++ src/navbar.js
@@ -1,39 +1,43 @@
-import React, { Component } from "react";
+import React from "react";
 import NavItem from "./navItem";
+import { motion, useMotionValueEvent, useScroll } from "framer-motion";
 
-class Navbar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <div
-        id="nav-bar"
-        className="flex fixed left-7 top-1/4 z-50 h-1/2 w-28 rounded-lg overflow-hidden text-lg font-inter"
-      >
-        <ul className="cursor-pointer grid grid-cols-1 w-full">
-          {/* To add border use 'border-l-4 border-transparent border-blue-400' */}
-          {/* <NavItem title="HOME" scrollspyId="section_1" active={true}></NavItem> */}
-          <NavItem
-            title="ABOUT ME"
-            scrollspyId="section_2"
-            active={false}
-          ></NavItem>
-          <NavItem
-            title="PROJECTS"
-            scrollspyId="section_3"
-            active={false}
-          ></NavItem>
-          <NavItem
-            title="CONTACT"
-            scrollspyId="section_4"
-            active={false}
-          ></NavItem>
-        </ul>
-      </div>
-    );
-  }
+export default function Navbar() {
+  // const { scrollYProgress } = useScroll();
+
+  // useMotionValueEvent(scrollYProgress, "change", (latest) => {
+  //   console.log("Page scroll: ", latest);
+  // });
+
+  return (
+    <motion.div
+      id="nav-bar"
+      className="navbar sticky inset-y-0 ml-20 pt-40 text-lg font-inter"
+      // style={{ opacity: scrollYProgress }}
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      viewport={{ amount: "all" }}
+      transition={{ duration: 1 }}
+    >
+      <motion.ul className="cursor-pointer gap-y-40 grid grid-cols-1 w-full">
+        {/* To add border use 'border-l-4 border-transparent border-blue-400' */}
+        {/* <NavItem title="HOME" scrollspyId="section_1" active={true}></NavItem> */}
+        <NavItem
+          title="ABOUT ME"
+          scrollspyId="section_2"
+          active={false}
+        ></NavItem>
+        <NavItem
+          title="PROJECTS"
+          scrollspyId="section_3"
+          active={false}
+        ></NavItem>
+        <NavItem
+          title="CONTACT"
+          scrollspyId="section_4"
+          active={false}
+        ></NavItem>
+      </motion.ul>
+    </motion.div>
+  );
 }
-
-export default Navbar;
